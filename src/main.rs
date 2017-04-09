f34c82b9a58ddf0982ee450d8f42beae784b746d
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-04-09 18:22:11 +0100

    Send to client

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,41 @@
-#![feature(rustc_private)]
-
+extern crate rustc_serialize;
 extern crate pnet;
 extern crate iron;
 extern crate staticfile;
 extern crate mount;
-extern crate rustc_serialize;
 
-use rustc_serialize::json;
 use std::path::Path;
 use std::thread;
-use std::sync::Mutex;
-use std::sync::Arc;
+use std::sync::{Mutex, Arc};
 
+use rustc_serialize::json::ToJson;
 use iron::{Iron, Request, Response};
 use staticfile::Static;
 use mount::Mount;
 
 use pnet::datalink::{self, NetworkInterface};
-use pnet::packet::Packet;
-use pnet::packet::arp::ArpPacket;
 use pnet::datalink::Channel::Ethernet;
-use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
-use pnet::packet::ipv4::Ipv4Packet;
-use pnet::packet::ipv6::Ipv6Packet;
-use std::net::IpAddr;
-
-fn handle_arp_packet(interface_name: &str, ethernet: &EthernetPacket) {
-    let header = ArpPacket::new(ethernet.payload());
-    if let Some(header) = header {
-        println!("[{}]: ARP packet: {}({}) > {}({}); operation: {:?}",
-			interface_name, ethernet.get_source(), header.get_sender_proto_addr(),
-            ethernet.get_destination(), header.get_target_proto_addr(), header.get_operation());
-    } else {
-        println!("[{}]: Malformed ARP Packet", interface_name);
-    }
-}
-
-fn handle_ipv4_packet(interface_name: &str, ethernet: &EthernetPacket) {
-    let header = Ipv4Packet::new(ethernet.payload());
-    if let Some(header) = header {
-        println!("{:?} {:?} {:?} {:?}", interface_name, IpAddr::V4(header.get_source()),
-        	IpAddr::V4(header.get_destination()), header.get_next_level_protocol());
-    } else {
-        println!("[{}]: Malformed IPv4 Packet", interface_name);
-    }
-}
-
-fn handle_ipv6_packet(interface_name: &str, ethernet: &EthernetPacket) {
-    let header = Ipv6Packet::new(ethernet.payload());
-    if let Some(header) = header {
-        println!("{:?} {:?} {:?} {:?}", interface_name, IpAddr::V6(header.get_source()),
-        	IpAddr::V6(header.get_destination()), header.get_next_header());
-    } else {
-        println!("[{}]: Malformed IPv6 Packet", interface_name);
-    }
-}
 
 fn main() {
     let interfaces = datalink::interfaces();
 
     for interface in interfaces.clone() {
         let mac = interface.mac.map(|mac| mac.to_string()).unwrap_or_else(|| "N/A".to_owned());
-        println!("{}({}) - {}:", interface.name, interface.index, interface.flags);
-        println!("\tMAC: {} -- {:?}", mac, interface.ips);
+        println!("{}({}) - {} -- {:?}", interface.name, interface.index, mac, interface.ips);
     }
 
-	let mut data = Arc::new(Mutex::new(Vec::new()));
+	let data = Arc::new(Mutex::new(Vec::new()));
 	let data_closure = data.clone();
 
-    // <http server>
     let mut mount = Mount::new();
 	mount
 		.mount("/", Static::new(Path::new("public")))
 		.mount("/data", move |_: &mut Request| {
-			let ref data2 = *data_closure.lock().expect("Unable to lock output");
-			Ok(Response::with((iron::status::Ok, json::encode(&data2).unwrap())))
+			let data2 = &(*data_closure.lock().expect("Unable to lock output"));
+			let json_data = data2.to_json();
+			Ok(Response::with((iron::status::Ok, json_data.to_string())))
 		});
-    thread::spawn(|| { Iron::new(mount).http("[::]:3000").unwrap(); });
-	// </http server>
+    thread::spawn(|| Iron::new(mount).http("[::]:3000").unwrap());
 
     let iface_name = "wlp2s0";
     let interface = interfaces.into_iter().find(|iface: &NetworkInterface| iface.name == iface_name).unwrap();
@@ -90,16 +48,11 @@ fn main() {
 
     let mut iter = rx.iter();
     loop {
-    	let mut data = data.lock().expect("Unable to lock output");
-    	data.push(iface_name);
-
         match iter.next() {
-            Ok(packet) => match packet.get_ethertype() {
-		        EtherTypes::Ipv4 => handle_ipv4_packet(iface_name, &packet),
-		        EtherTypes::Ipv6 => handle_ipv6_packet(iface_name, &packet),
-		        EtherTypes::Arp => handle_arp_packet(iface_name, &packet),
-		        _ => println!("[{}]: Unknown packet: {} > {}; ethertype: {:?} length: {}", iface_name,
-		        	&packet.get_source(), &packet.get_destination(), &packet.get_ethertype(), &packet.packet().len())
+            Ok(packet) => {
+    			let mut data = data.lock().expect("Unable to lock output");
+    			data.push(format!("{{\"src\": \"{:?}\", \"dst\": \"{:?}\", \"type\": \"{:?}\"}}",
+    				packet.get_source(), packet.get_destination(), packet.get_ethertype()));
 		    },
             Err(e) => panic!("packetdump: unable to receive packet: {}", e),
         }
