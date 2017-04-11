f26ede3b2180b51242278f405ae2a1c43625589b
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-04-11 18:40:56 +0100

    Indentation

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ use std::collections::BTreeMap;
 use std::io::{self, Write};
 use std::sync::{Mutex, Arc};
 
-use rustc_serialize::json::ToJson;
+use rustc_serialize::json::{ToJson, Json};
 use iron::{Iron, Request, Response};
 use staticfile::Static;
 use mount::Mount;
 
 use pnet::packet::Packet;
-use pnet::packet::ethernet::EtherTypes;
+use pnet::packet::ethernet::{EtherTypes, EtherType, EthernetPacket};
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::arp::ArpPacket;
@@ -71,21 +71,21 @@ impl <'a> CommStore {
 }
 
 fn main() {
-    let iface_name = env::args().nth(1).unwrap_or_else(|| {
-        writeln!(io::stderr(), "[!] Usage: passive-packet <interface>").unwrap();
-        process::exit(1);
-    });
+	let iface_name = env::args().nth(1).unwrap_or_else(|| {
+		writeln!(io::stderr(), "[!] Usage: passive-packet <interface>").unwrap();
+		process::exit(1);
+	});
 
-    let interface = datalink::interfaces().into_iter()
-    					.find(|iface: &NetworkInterface| iface.name == iface_name)
-    					.unwrap_or_else(|| {
-    						writeln!(io::stderr(), "[!] That interface does not exist.").unwrap();
-        					process::exit(1);
-    					});
+	let interface = datalink::interfaces().into_iter()
+						.find(|iface: &NetworkInterface| iface.name == iface_name)
+						.unwrap_or_else(|| {
+							writeln!(io::stderr(), "[!] That interface does not exist.").unwrap();
+							process::exit(1);
+						});
 
-	let data = Arc::new(Mutex::new(Vec::new()));
+	let data = Arc::new(Mutex::new(CommStore::new()));
 	let data_closure = data.clone();
-    let mut mount = Mount::new();
+	let mut mount = Mount::new();
 
 	mount.mount("/", Static::new(Path::new("public"))).mount("/data", move |_: &mut Request| {
 		let data2 = &(*data_closure.lock().expect("Unable to lock output"));
@@ -93,57 +93,56 @@ fn main() {
 		Ok(Response::with((iron::status::Ok, json_data.to_string())))
 	});
 
-    thread::spawn(|| Iron::new(mount).http("[::]:3000").unwrap());
-    println!("Listening...");
+	thread::spawn(|| Iron::new(mount).http("[::]:3000").unwrap());
+	println!("Listening...");
 
-    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("[!] Unhandled channel type"),
-        Err(e) => panic!("[!] Unable to create channel: {}", e),
-    };
+	let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+		Ok(Ethernet(tx, rx)) => (tx, rx),
+		Ok(_) => panic!("[!] Unhandled channel type"),
+		Err(e) => panic!("[!] Unable to create channel: {}", e),
+	};
 
-    let mut iter = rx.iter();
-    let mut i = 0;
-    loop {
-        match iter.next() {
-            Ok(packet) => {
-            	i += 1;
-            	if i % 100 == 0 {
-            		println!("Captured {} packets.", i);
-            	}
-    			let mut data = data.lock().expect("Unable to lock output");
+	let mut iter = rx.iter();
+	let mut i = 0;
+	loop {
+		match iter.next() {
+			Ok(packet) => {
+				i += 1;
+				if i % 100 == 0 {
+					println!("Captured {} packets.", i);
+				}
+				let mut data = data.lock().expect("Unable to lock output");
 
-    			let (src, dst): (String, String) = match packet.get_ethertype() {
-			        EtherTypes::Ipv4 => {
-			        	let header = Ipv4Packet::new(packet.payload());
-    					if let Some(header) = header {
-							(header.get_source().to_string(), header.get_destination().to_string())
-						} else {
-							("N/A".to_string(), "N/A".to_string())
-						}
-			        },
-			        EtherTypes::Ipv6 => {
-			        	let header = Ipv6Packet::new(packet.payload());
-    					if let Some(header) = header {
+				let (src, dst): (String, String) = match packet.get_ethertype() {
+					EtherTypes::Ipv4 => {
+						let header = Ipv4Packet::new(packet.payload());
+						if let Some(header) = header {
 							(header.get_source().to_string(), header.get_destination().to_string())
 						} else {
 							("N/A".to_string(), "N/A".to_string())
 						}
 					},
-			        EtherTypes::Arp => {
-			        	let header = ArpPacket::new(packet.payload());
-    					if let Some(header) = header {
+					EtherTypes::Ipv6 => {
+						let header = Ipv6Packet::new(packet.payload());
+						if let Some(header) = header {
+							(header.get_source().to_string(), header.get_destination().to_string())
+						} else {
+							("N/A".to_string(), "N/A".to_string())
+						}
+					},
+					EtherTypes::Arp => {
+						let header = ArpPacket::new(packet.payload());
+						if let Some(header) = header {
 							(header.get_sender_proto_addr().to_string(), header.get_target_proto_addr().to_string())
 						} else {
 							("N/A".to_string(), "N/A".to_string())
 						}
 					},
-			        _ => ("N/A".to_string(), "N/A".to_string())
-			    };
-
-    			data.push(format!("{{\"src\": {:?}, \"dst\": {:?}, \"type\": \"{:?}\"}}", src, dst, packet.get_ethertype()));
-		    },
-            Err(e) => panic!("[!] Unable to receive packet: {}", e),
-        }
-    }
+					_ => ("N/A".to_string(), "N/A".to_string())
+				};
+				data.add(src, dst, packet);
+			},
+			Err(e) => panic!("[!] Unable to receive packet: {}", e),
+		}
+	}
 }
